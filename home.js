d6879abcc418d5bb91d5793f0f55df40d73e0ec3
Edwin Torres <edwin.torres.jr2@gmail.com>
2020-01-16 16:05:11 -0500

    text updates and navbar

--- home.js
+++ home.js
@@ -1,7 +1,29 @@
 console.log("start")
-let navButton = document.querySelector(".mobileNavButton")
-let mobileNav = document.querySelector(".mobileNav")
-let closeButton = document.querySelector(".closeButton")
+const navButton = document.querySelector(".mobileNavButton")
+const mobileNav = document.querySelector(".mobileNav")
+const closeButton = document.querySelector(".closeButton")
+const bio = document.querySelector(".bio")
+const about = document.getElementById("about")
+const projects = document.getElementById("projects")
+const projectlist = document.querySelector(".projects")
+const contact = document.getElementById("contact")
+const cta = document.getElementById("cta")
+
+
+contact.addEventListener("click", e => {
+    e.preventDefault()
+    cta.scrollIntoView(true)
+})
+
+about.addEventListener("click", e => {
+    e.preventDefault()
+    bio.scrollIntoView(true)
+})
+
+projects.addEventListener("click", e => {
+    e.preventDefault()
+    projectlist.scrollIntoView(true)
+})
 
 navButton.addEventListener("click", (event) => {
     navButton.classList.toggle("hidden")
